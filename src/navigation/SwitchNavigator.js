938cd6a354fd5f804214356384bd1cd08db0b465
Ilya Hamisoniya <Ildoradogam@gmail.com>
2019-12-12 14:07:20 +0300

    feat: switchNavigator animation

--- src/navigation/SwitchNavigator.js
+++ src/navigation/SwitchNavigator.js
@@ -1,21 +1,18 @@
 import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+import createAnimatedSwitchNavigator from 'react-navigation-animated-switch';
+
 import {Transition} from 'react-native-reanimated';
 import React from 'react';
 import AuthScreen from '../screens/AuthScreen';
 import DrawerNavigator from './DrawerNavigator';
 export default createAppContainer(
-  createSwitchNavigator(
+  createAnimatedSwitchNavigator(
     {
       App: DrawerNavigator,
       Auth: AuthScreen,
     },
     {
-      transition: <Transition.Out type="slide-bottom" durationMs={400} />,
-      // transition: (
-      //   <Transition.Together>
-      //     <Transition.Out type="slide-bottom" durationMs={400} />
-      //     <Transition.In type="fade" durationMs={500} />
-      //   </Transition.Together>
+      transition: <Transition.In type="scale" durationMs={500} />,
       initialRouteName: 'Auth',
     },
   ),
